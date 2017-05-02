c4768252933ea64ad4912ddbacfcdc2699b4bba4
rkputraa <rkputraa@gmail.com>
2017-05-02 11:19:14 +0700

    belajar include dan require

---
+++ belajar/tabel.php
@@ -0,0 +1,21 @@
+<table border="3" cellspacing="0" cellpadding="10">
+<thead>
+<tr >
+	<th>no</th>
+	<th>nim</th>
+	<th>nama</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+	<td>1</td>
+	<td>h1101151003</td>
+	<td>Rachmat</td>
+</tr>
+<tr>
+	<td>2</td>
+	<td>h11011510000</td>
+	<td>wak</td>
+</tr>
+</tbody>
+</table>
